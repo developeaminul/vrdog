645f33c58ba13a311e08fc53a2266d12e8e635be
Md Sharif Miah <sharif.golang@gmail.com>
2022-08-20 23:30:32 +0600

    slider added

--- silder/silder.js
+++ silder/silder.js
@@ -1,53 +1,72 @@
 
 
-const swiperParams = {
-   slidesPerView: 1,
-   spaceBetween: 30,
-   centeredSlides: true,
-   grabCursor: true,
-   loop: true,
-   loopFillGroupWithBlank: true,
-   // effect: "fade",
-   autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
-};
+// const swiperParams = {
+//    slidesPerView: 1,
+//    spaceBetween: 30,
+//    centeredSlides: true,
+//    grabCursor: true,
+//    loop: true,
+//    loopFillGroupWithBlank: true,
+//    // effect: "fade",
+//    autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//    pagination: {
+//      el: ".swiper-pagination",
+//      clickable: true,
+//    },
+//    navigation: {
+//      nextEl: ".swiper-button-next",
+//      prevEl: ".swiper-button-prev",
+//    },
+// };
 
-const vrdogBanner = new Swiper('.vrdog__slider', swiperParams);
-const vrdogTestimonial = new Swiper('.swiper__testimonial', swiperParams,);
-
-
+// const vrdogBanner = new Swiper('.vrdog__slider', swiperParams);
+// const vrdogTestimonial = new Swiper('.swiper__testimonial', swiperParams,);
 
 
-/**
- * 
- 
-var swiper = new Swiper(".vrdog__slider", {
-   slidesPerView: 1,
-   spaceBetween: 30,
-   centeredSlides: true,
-   grabCursor: true,
-   loop: true,
-   loopFillGroupWithBlank: true,
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
- });
 
- * 
- */
+
+var slider1 = new Swiper ('.vrdog__slider', {
+  effect: 'cube',
+  
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+var slider2 = new Swiper ('.swiper__testimonial', {
+  
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
