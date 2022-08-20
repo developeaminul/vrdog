cc3bde2d0a3004b91eb620bfd708946edc3c6a3f
Md Aminul Islam <mdaminulislam2015bd@gmail.com>
2022-08-20 22:37:48 +0600

    Slider

--- silder/silder.js
+++ silder/silder.js
@@ -1,3 +1,36 @@
+
+
+const swiperParams = {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   centeredSlides: true,
+   grabCursor: true,
+   loop: true,
+   loopFillGroupWithBlank: true,
+   // effect: "fade",
+   autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+};
+
+const vrdogBanner = new Swiper('.vrdog__slider', swiperParams);
+const vrdogTestimonial = new Swiper('.swiper__testimonial', swiperParams,);
+
+
+
+
+/**
+ * 
+ 
 var swiper = new Swiper(".vrdog__slider", {
    slidesPerView: 1,
    spaceBetween: 30,
@@ -14,3 +47,7 @@ var swiper = new Swiper(".vrdog__slider", {
      prevEl: ".swiper-button-prev",
    },
  });
+
+ * 
+ */
+
